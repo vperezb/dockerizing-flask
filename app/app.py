10f32fcc3ef74eaf1ba0587d62b99c8b3dab55b3
vperezb <victor.perez@tiendeo.com>
2019-04-09 11:51:34 +0200

    Push sender first commit

--- app/app.py
+++ app/app.py
@@ -1,8 +1,49 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from scripts import sender_presenter as sp
 # import os HOME_DIR = os.getenv('HOME_DIR')
 
+import json
+
+def read_json(path):
+    with open('strings.json') as f:
+        return json.load(f)
+
+
 app = Flask(__name__)
+
+@app.route('/push_sender')
+def sender():
+    return render_template('push_sender.html')
+
+
+@app.route('/send_push_by_token', methods=['POST'])
+def send_push_by_token():
+    push_engine = sp.__initialize_app(r'app\data\firebase_secrets\PRO.json')
+    if not request.form['push_data']:
+        push_data = read_json(r'app\data\default_push_data.json')
+    else:
+        push_data = request.form['push_data']
+    response = sp.send_to_token(request.form['fcm_token'], push_data, push_engine, False)
+    return (response)
+
+@app.route('/suscribe_token_to_topic', methods=['POST'])
+def suscribe_token_to_topic():
+    push_engine = sp.__initialize_app(r'app\data\firebase_secrets\PRO.json')
+    response = sp.subscribe_to_topic(request.form['topic'], [request.form['fcm_token']], push_engine)
+    return (response)
+
+
+@app.route('/send_push_by_topic', methods=['POST'])
+def send_push_by_topic():
+    push_engine = sp.__initialize_app(r'app\data\firebase_secrets\PRO.json')
+    if not request.form['push_data']:
+        push_data = read_json(r'app\data\default_push_data.json')
+    else:
+        push_data = request.form['push_data']
+    response = sp.send_to_topic(request.form['fcm_topic'], push_data, push_engine, False)
+    return (response)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///C:\\Users\\Orwell\\Documents\\GitProjects\\dockerizing-flask\\sqlite.db'
 
@@ -16,13 +57,6 @@ class Firmas(db.Model):
     nombre = db.Column(db.String(30))
     comentario = db.Column(db.String(5000))
 
-
-@app.route('/')
-def landing():
-    result = Firmas.query.order_by(Firmas.id.desc()).all()
-    return render_template('index.html', result=result)
-
-
 @app.route('/firma/', methods=['GET', 'POST'])
 def firma():
     if request.method == 'GET':
@@ -35,7 +69,16 @@ def firma():
         db.session.add(firma)
         db.session.commit()
 
-        return redirect(url_for('landing    '))
+        return redirect(url_for('landing'))
+
+@app.route('/result_firmas')
+def landing():
+    result = Firmas.query.order_by(Firmas.id.desc()).all()
+    return render_template('index.html', result=result)
+
+@app.route('/base')
+def base_html():
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
